f84d5b40ca584bebbe91939bf96f0bfb6a5b7f4e
bauassr <bauassr@gmail.com>
2018-03-13 12:19:42 +0530

    Add files via upload

---
+++ Assignment_2.1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 16:06:32 2018
+
+@author: singh.shivam
+"""
+print("Enter numbers seperated with comma:")
+
+List=list(map(int,input().split(",")))
+for L in List:
+    print(L)
+    
+
+
